c06bedd03a89e4d216e997968cf8fef84ed41439
Cris Mollee <crismollee@live.com>
2018-01-24 15:01:25 +0100

    Three sensors working with smoother and NewPing library.

--- ultrasonicSensor/ultrasonicSensor.ino
+++ ultrasonicSensor/ultrasonicSensor.ino
@@ -13,28 +13,55 @@
 #include "floatsmoothing.h"
 #include <NewPing.h>
 
-FloatSmoothing distanceSm(0.05);
 
 #define TRIGGER_PIN 8
 #define ECHO_PIN 9
-#define MAX_DISTANCE 450
-float distanceSmooth;
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-// NewPing setup of pins and maximum distance.
+#define TRIGGER_PIN2 2
+#define ECHO_PIN2 3
+
+#define TRIGGER_PIN3 4
+#define ECHO_PIN3 5
+
+#define MAX_DISTANCE 450
+
+
+float distanceSmooth1;
+float distanceSmooth2;
+float distanceSmooth3;
+
+FloatSmoothing distanceSm1(0.1);
+FloatSmoothing distanceSm2(0.1);
+FloatSmoothing distanceSm3(0.1);
+
+NewPing S1(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+NewPing S2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
+NewPing S3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE);
+
+unsigned int uS1;
+unsigned int uS2;
+unsigned int uS3;
 
 void setup() {
-
   Serial.begin (9600);
-
 }
 
 
 void loop() {
+  uS1 = S1.ping_cm();
+  uS2 = S2.ping_cm();
+  uS3 = S3.ping_cm();
 
-  delay(50);
-  unsigned int uS = sonar.ping_cm();
-  distanceSmooth = distanceSm.smooth(uS);
-  Serial.println(distanceSmooth);
+  distanceSmooth1 = distanceSm1.smooth(uS1);
+  distanceSmooth2 = distanceSm2.smooth(uS2);
+  distanceSmooth3 = distanceSm3.smooth(uS3);
 
+  Serial.print("0 600 ");
+  Serial.print(distanceSmooth1);
+  Serial.print("\t");
+  Serial.print(distanceSmooth2);
+  Serial.print("\t");
+  Serial.println(distanceSmooth3);
+
+  delay(25);
 }
